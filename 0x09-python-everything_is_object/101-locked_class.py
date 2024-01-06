3afa74033341a41ab91756b16049c59785289031
abbasol <abbas_fu@outlook.sa>
2024-01-06 12:39:30 -0500

    object9

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-""" LockedClass
-"""
+""" LockedClass."""
 
 
 class LockedClass:
+    """
+    prevent the user from instantiating new LockedClass attributes
+    for anything but attributes called 'first_name'.
+    """
+
     __slots__= ['first_name']
