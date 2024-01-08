515736070e91662a01f5bb5c37603679910606fa
abbasol <abbas_fu@outlook.sa>
2024-01-08 07:17:53 -0500

    inheritance

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""" returns the list of available attributes
+"""  returns the list of available attributes
 and methods of an object
 """
 
 
-def loockup(obj):
+def lookup(obj):
     """returns the list of available attributes"""
     return dir(obj)
