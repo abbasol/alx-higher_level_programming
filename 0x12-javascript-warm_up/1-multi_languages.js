b7ea785f7a58ed88578a5c80cfca0b25964162fe
abbasol <abbas_fu@outlook.sa>
2024-03-13 08:04:55 -0400

    done

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
-const myVar = 'JavaScript is amazing';
-console.log(myVa#!/usr/bin/node
 console.log('C is fun');
 console.log('Python is cool');
-console.log('JavaScript is amazing');r);
+console.log('JavaScript is amazing');
